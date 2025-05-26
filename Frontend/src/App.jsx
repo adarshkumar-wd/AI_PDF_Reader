dc90c352ae1dc41d8a97e1945000947ff4a10c23
Adarsh Kumar <adarshff06@gmail.com>
2025-05-26 10:22:28 +0530

    nav baar completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiSquarePlus } from "react-icons/ci";
+""
 
 function App() {
-  
+
+  const [isImageUpload, setIsImageUpload] = useState(false);
+
   return (
-    <div className='bg-red-500'>App</div>
+    <>
+      <nav className='w-full h-20% flex py-3 px-2 justify-between items-center shadow-lg'>
+
+        {/* logo */}
+
+        <div className='w-[20%]'>
+          <img className='' src="https://static.swapcard.com/public/images/c5314c79a3d74b5eb0672dd534babac5.png" alt="" />
+        </div>
+
+        <div className='flex gap-4'>
+          {/* upload Immage option */}
+
+          <label className='font-bold text-2xl' for="file"><CiSquarePlus /></label>
+
+          <div>
+            <input onChange={() => setIsImageUpload(!isImageUpload)} type="file" id='file' className={`${isImageUpload ? "" : "hidden"}`} />
+          </div>
+
+        </div>
+      </nav>
+    </>
   )
 }
 
