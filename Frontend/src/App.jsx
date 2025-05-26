2aa58e31c99577961c5b8dfee7020507d96190bf
Adarsh Kumar <adarshff06@gmail.com>
2025-05-26 12:08:09 +0530

    Connection with backend Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,45 @@
 import React, { useState } from 'react'
 import { CiSquarePlus } from "react-icons/ci";
 import { VscSend } from "react-icons/vsc";
+import axios from "axios"
 ""
 
 function App() {
 
   const [isImageUpload, setIsImageUpload] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [chats, setChats] = useState([{}]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await axios.post("http://localhost:8000/api/ask-question", { question: question });
+    console.log(response.data.answer)
+    setQuestion("");
+  }
+
+  const handlePDF = async (e) => {
+    e.preventDefault();
+
+    const file = e.target.files[0];
+    if (!file) return;
+    setFileName(file.name)
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/upload-pdf", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log("Upload successful:", response.data);
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
+  };
 
   return (
     <div className='h-screen'>
@@ -18,13 +52,14 @@ function App() {
           <img className='' src="https://static.swapcard.com/public/images/c5314c79a3d74b5eb0672dd534babac5.png" alt="" />
         </div>
 
-        <div className='flex gap-4'>
+        <div className='flex gap-4 items-center'>
           {/* upload Immage option */}
 
+          <p className='font-semibold text-[.7rem] text-green-500'>{fileName}</p>
           <label className='font-bold text-3xl' htmlFor="file"><CiSquarePlus /></label>
 
           <div>
-            <input onChange={() => setIsImageUpload(!isImageUpload)} type="file" id='file' className={`${isImageUpload ? "" : "hidden"}`} />
+            <input onChange={handlePDF} type="file" id='file' className={`${isImageUpload ? "" : "hidden"}`} />
           </div>
 
         </div>
@@ -38,13 +73,24 @@ function App() {
 
         <div className='flex bg-gray-100 items-center w-[90%]  border-1 border-gray-200 px-3 py-1 rounded-md'>
 
-          <input type="text" placeholder='Enter a message...' className='w-full outline-none px-2 py-1 font-light'/>
-          <VscSend className=''/>
+
+
+          <input
+            type="text"
+            placeholder='Enter a message...'
+            className='w-full outline-none px-2 py-1 font-light'
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+
+          <button onClick={handleSubmit}>
+            <VscSend className='' />
+          </button>
+
 
         </div>
 
       </footer>
-      
+
 
     </div>
   )
