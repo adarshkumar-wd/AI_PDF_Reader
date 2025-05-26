4f5624fd284730f447d8e59677434d7b75f84851
Adarsh Kumar <adarshff06@gmail.com>
2025-05-26 10:50:10 +0530

    basic UI completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { CiSquarePlus } from "react-icons/ci";
+import { VscSend } from "react-icons/vsc";
 ""
 
 function App() {
@@ -7,8 +8,9 @@ function App() {
   const [isImageUpload, setIsImageUpload] = useState(false);
 
   return (
-    <>
-      <nav className='w-full h-20% flex py-3 px-2 justify-between items-center shadow-lg'>
+    <div className='h-screen'>
+
+      <nav className='w-full h-[10%] flex py-3 px-2 justify-between items-center shadow-lg'>
 
         {/* logo */}
 
@@ -19,7 +21,7 @@ function App() {
         <div className='flex gap-4'>
           {/* upload Immage option */}
 
-          <label className='font-bold text-2xl' for="file"><CiSquarePlus /></label>
+          <label className='font-bold text-3xl' htmlFor="file"><CiSquarePlus /></label>
 
           <div>
             <input onChange={() => setIsImageUpload(!isImageUpload)} type="file" id='file' className={`${isImageUpload ? "" : "hidden"}`} />
@@ -27,7 +29,24 @@ function App() {
 
         </div>
       </nav>
-    </>
+
+      <section className='w-full min-h-[80%] bg--100'>
+
+      </section>
+
+      <footer className='w-full bg-white shadow-[0_-10px_6px_-1px_rgba(0,0,0,0.1)] h-[10%] flex items-center justify-center'>
+
+        <div className='flex bg-gray-100 items-center w-[90%]  border-1 border-gray-200 px-3 py-1 rounded-md'>
+
+          <input type="text" placeholder='Enter a message...' className='w-full outline-none px-2 py-1 font-light'/>
+          <VscSend className=''/>
+
+        </div>
+
+      </footer>
+      
+
+    </div>
   )
 }
 
